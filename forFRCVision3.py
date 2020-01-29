5d81fd4dda46dc9f0f2274286b98aceb339b4b46
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-01-29 17:12:00 -0500

    fix opening morphop

--- forFRCVision3.py
+++ forFRCVision3.py
@@ -109,8 +109,8 @@ def main(config):
         #Threshold HSV Colorspace (Only allow yellow ball color)
         hsv_frame = cv2.inRange(hsv_frame, lower_hsv, upper_hsv)
         #Open to eliminate noise
-        hsv_frame = cv2.erode(hsv_frame, anti_noise_kernel, iterations = 2)
-        hsv_frame = cv2.dilate(hsv_frame, anti_noise_kernel, iterations = 2)
+        hsv_frame = cv2.erode(hsv_frame, anti_noise_kernel)
+        hsv_frame = cv2.dilate(hsv_frame, anti_noise_kernel)
         #Close to fill in the logo
         hsv_frame = cv2.dilate(hsv_frame, anti_logo_kernel)
         hsv_frame = cv2.erode(hsv_frame, anti_logo_kernel)
