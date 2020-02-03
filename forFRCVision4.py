59114acee2b565f4eb27ba5efa8c36bd1e05ce85
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-02-03 17:04:19 -0500

    revert to reasonable morphops

--- forFRCVision4.py
+++ forFRCVision4.py
@@ -26,11 +26,11 @@ lower_lab = np.array([150,100,170])
 upper_lab = np.array([250,150,200])
 #Define morphological operation kernels
 anti_noise_kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
-anti_logo_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+anti_logo_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
 anti_lighting_anomaly_kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
 final_anti_noise_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 final_desegmentation_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-final_open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19,19))
+final_open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11,11))
 #edt_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
 #circle_improvement_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 
@@ -142,12 +142,11 @@ def main(config):
         hsv_frame = cv2.erode(hsv_frame, final_anti_noise_kernel, iterations = 2)
         hsv_frame = cv2.dilate(hsv_frame, final_anti_noise_kernel, iterations = 2)
         #Close to fix any leftover artificial segmentation issues from previous opening
-        hsv_frame = cv2.dilate(hsv_frame, final_desegmentation_kernel)
-        hsv_frame = cv2.erode(hsv_frame, final_desegmentation_kernel)
+        hsv_frame = cv2.dilate(hsv_frame, final_desegmentation_kernel, iterations = 2)
+        hsv_frame = cv2.erode(hsv_frame, final_desegmentation_kernel, iterations = 2)
         #Open to allow watershed to function
-        hsv_frame = cv2.erode(hsv_frame, final_open_kernel, iterations = 2)
-        imageog = cv2.dilate(hsv_frame, final_open_kernel)
-        imagefm = cv2.erode(imageog, final_open_kernel)
+        imagefm = cv2.erode(hsv_frame, final_open_kernel, iterations = 2)
+        imageog = cv2.dilate(imagefm, final_open_kernel)
         #Convert Colorspace for watershed
         imageo = cv2.cvtColor(imageog, cv2.COLOR_GRAY2BGR)
         #Watershed image segmentation
