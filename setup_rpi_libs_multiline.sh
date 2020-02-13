445183a0ecca5214ffa7fe7207575d0d5f58a7ce
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-02-12 19:57:50 -0500

    echo when done

--- setup_rpi_libs_multiline.sh
+++ setup_rpi_libs_multiline.sh
@@ -2,6 +2,8 @@
 sudo cd /
 echo "INFO: Fixing Libraries, please press Y or enter the root password when asked."
 sleep 2
+echo "INFO: Please do not turn off or shutdown until complete. "
+sleep 2
 sudo mount -o remount, rw /
 sudo apt-get uninstall python
 sudo apt-get uninstall python3
@@ -18,3 +20,4 @@ sudo pip3 install opencv
 sudo pip3 install imutils
 sudo pip3 install pillow
 sudo mount -o remount r /etc/modprobe.d
+echo "INFO: Complete. "
