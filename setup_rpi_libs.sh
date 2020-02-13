ffaeee7f5dc82b2d6c1865fc1f72bf263559b569
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-02-12 19:59:34 -0500

    match multiline version

--- setup_rpi_libs.sh
+++ setup_rpi_libs.sh
@@ -1,17 +1,1 @@
-sudo cd /;
-sudo mount -o remount, rw /;
-sudo apt-get uninstall python;
-sudo apt-get uninstall python3;
-sudo apt-get uninstall python-numpy;
-sudo apt-get uninstall python3-numpy;
-sudo apt-get uninstall python-pip;
-sudo apt-get uninstall python3-pip;
-sudo apt-get install python3;
-sudo apt-get install python3-pip;
-sudo pip3 install numpy;
-sudo pip3 install scipy;
-sudo pip3 install scikit-image;
-sudo pip3 install opencv;
-sudo pip3 install imutils;
-sudo pip3 install pillow;
-sudo mount -o remount r /etc/modprobe.d;
+sudo cd /;echo "INFO: Fixing Libraries, please press Y or enter the root password when asked.";sleep 2;echo "INFO: Please do not turn off or shutdown until complete. ";sleep 2;sudo mount -o remount, rw /;sudo apt-get uninstall python;sudo apt-get uninstall python3;sudo apt-get uninstall python-numpy;sudo apt-get uninstall python3-numpy;sudo apt-get uninstall python-pip;sudo apt-get uninstall python3-pip;sudo apt-get install python3;sudo apt-get install python3-pip;sudo pip3 install numpy;sudo pip3 install scipy;sudo pip3 install scikit-image;sudo pip3 install opencv;sudo pip3 install imutils;sudo pip3 install pillow;sudo mount -o remount r /etc/modprobe.d;echo "INFO: Complete. "
